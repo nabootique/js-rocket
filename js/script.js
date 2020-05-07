526f561b6c1db96afce77851beef18805573f030
nabootique <64191711+nabootique@users.noreply.github.com>
2020-05-07 21:23:21 +1000

    Made The Rocket Move

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-var car = {
-  make: 'Lamborghini',
-  type: 'Gallardo',
-  color: 'Yellow',
-  isTurnedOn: false,
-  numberOfWheels: 4,
-  seats: ['seat1', 'seat2', 'seat3', 'seat4'],
-  turnOn: function() {
-    this.isTurnedOn = true;
-  }
-};
+var timer = null;
+var countdownNumber = 10;
 
-console.log('Hello There!');
+var changeState = function(state) {
+    document.body.className = 'body-state'+state;
+        clearInterval(timer);
+        countdownNumber = 10;
+        document.getElementById('countdown').innerHTML =
+        countdownNumber;
+
+    if (state == 2) {
+      timer == setInterval(function () {
+          countdownNumber = countdownNumber-1;
+          document.getElementById('countdown').innerHTML =
+          countdownNumber;
+          if (countdownNumber <= 0) {
+            changeState(3)
+          };
+      }, 500);
+    } else if (state == 3) {
+        var success = setTimeout(function ()
+            {
+                  var randomNumber = Math.round(Math.random()*10);
+
+                  console.log('randomNumber:', randomNumber)
+                  //success
+                  if(randomNumber > 5) {
+                    changeState(4);
+
+                  } else {
+                    changeState(5);
+                  }
+        }, 2000);
+    }
+}
