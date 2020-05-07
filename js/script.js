bce89cb14a1346fc66aaf72f9a7727640ac2d921
nabootique <64191711+nabootique@users.noreply.github.com>
2020-05-07 17:12:53 +1000

    Making Changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var car = {
+  make: 'Lamborghini',
+  type: 'Gallardo',
+  color: 'Yellow',
+  isTurnedOn: false,
+  numberOfWheels: 4,
+  seats: ['seat1', 'seat2', 'seat3', 'seat4'],
+  turnOn: function() {
+    this.isTurnedOn = true;
+  }
+};
+
+console.log('Hello There!');
